435d77b05584eadf3b713ac9c7711c6eee33373c
Daniel L <khleungaw@connect.ust.hk>
2024-03-13 10:09:06 +0800

    connect backend and frontend

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
 # from camera import capture_and_upload_frames
+from inference import infer
+from face_to_encoding import encodeSet, encodeByPerson
+from train import train
 
 app = Flask(__name__)
 
@@ -15,11 +18,21 @@ app.secret_key = 'test'
 # Assuming we have a single user for demonstration purposes
 # In a real-world scenario, you would use a database
 user_info = {
-    "admin" : generate_password_hash("password123")  #temp, should not Never store passwords in plain text
+    "admin" : generate_password_hash("password123"),  #temp, should not Never store passwords in plain text
+    "Justin_Sun": generate_password_hash("password123"),
 }
 
 UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'image')
+ENCODINGS_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources', 'encodings.txt')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ENCODINGS_PATH'] = ENCODINGS_PATH
+
+# Generate model using encodings
+if not os.path.exists(app.config['ENCODINGS_PATH']):
+    encodeSet(UPLOAD_FOLDER, app.config['ENCODINGS_PATH'])
+    train(app.config['ENCODINGS_PATH'])
+else:
+    train(app.config['ENCODINGS_PATH'])
 
 @app.route('/')
 def index():
@@ -53,7 +66,12 @@ def login():
 @app.route('/faceID', methods=['GET','POST'])
 def faceID():
     if request.method == 'POST':
+        if 'username' not in session:
+            return redirect(url_for('login'))
+        username = session['username']
+
         frames = []
+        testFileName = "" # Temporary solution, should be changed
         for i in range(len(request.files)):
             frame = request.files['frame' + str(i)]
             filename = secure_filename(frame.filename)
@@ -66,11 +84,16 @@ def faceID():
             with open(filepath, 'wb') as file:
                 file.write(frame.read())
             frames.append(frame)
+
+            testFileName = filepath # Temporary solution, should be changed
        
         # For demonstration assumin authentication is successful
-        if frames:
+        result = infer(testFileName)
+        if (result != None and result[0] == username):
+            print('Face recognized for '+ username)
             return jsonify({'redirect': url_for('home')})  # Return JSON response with redirect URL
         else:
+            print('Face not recognized')
             return jsonify({'message': 'Face not recognized'})  # Return JSON response with message
     else:
         return render_template('faceid.html')
@@ -97,14 +120,20 @@ def register():
             timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
             filename = f"{email}_{timestamp}_{index}.{frame.filename.split('.')[-1]}"
             
-            save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'], email), exist_ok=True)
+            save_path = os.path.join(app.config['UPLOAD_FOLDER'], email, filename)
             frame.save(save_path)
             frames.append(frame)
             index += 1
-        
+
         if email and password and frames:
             # check if the face registration in register.html is successful
             success = True
+
+            # Initailize models
+            encodeByPerson(app.config['UPLOAD_FOLDER'], email, app.config['ENCODINGS_PATH'])
+            train(app.config['ENCODINGS_PATH'])
+            
             if success:
                 message = 'Registration successful!'
             else:
---
+++ inference.py
@@ -0,0 +1,30 @@
+import pickle
+import os
+import numpy as np
+import face_recognition
+import face_to_encoding
+
+def infer(imgpath):
+    if not face_to_encoding.checkFace(imgpath):
+        return None
+    else:
+        face = face_recognition.load_image_file(imgpath)
+        face_enc = face_recognition.face_encodings(face)[0]
+        with open('face_classifier.pkl', 'rb') as fid:
+            clf = pickle.load(fid)
+            p = clf.predict_proba(face_enc.reshape(1, -1))
+            print(p)
+            label_i = np.argmax(p, axis=1)
+            if p[0][label_i] >= 0.95:
+                return clf.predict(face_enc.reshape(1, -1))
+            else:
+                return "No Match"
+
+def loginInfer(imgdir, label):
+    matching_count = 0
+    pix = os.listdir(imgdir)
+    for img in pix:
+        if infer(imgdir + '/' + img) == label:
+            matching_count += 1
+    return matching_count >= 9
+
